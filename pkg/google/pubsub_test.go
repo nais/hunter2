2aa6d872ffee9c6d3e6c87b8cf1e7f7dc4c3445d
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2020-11-27 11:57:22 +0100

    test: add tests for parsing secret version from pubsub payload

--- pkg/google/pubsub_test.go
+++ pkg/google/pubsub_test.go
@@ -1,26 +1,32 @@
 package google_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/magiconair/properties/assert"
 	"github.com/nais/hunter2/pkg/google"
 )
 
-type secretnametest struct {
+type testcase struct {
 	input  string
 	output string
 	err    error
 }
 
-var secretNameTests = []secretnametest{
+var secretNameTests = []testcase{
 	{
 		input:  "projects/12345/secrets/foobar",
 		output: "foobar",
 	},
 	{
-		input:  "projects/12345/secrets/foobar/versions/NEW",
+		input:  "projects/12345/secrets/foobar/versions/2",
 		output: "foobar",
+	},
+	{
+		input: "projects/12345/secrets",
+		output: "",
+		err: fmt.Errorf("resource name does not contain a secret"),
 	},
 }
 
@@ -29,5 +35,27 @@ func TestParseSecretName(t *testing.T) {
 		output, err := google.ParseSecretName(test.input)
 		assert.Equal(t, test.output, output)
 		assert.Equal(t, test.err, err)
+	}
+}
+
+var secretVersionTests = []testcase{
+	{
+		input:  "projects/12345/secrets/foobar",
+		output: "1",
+	},
+	{
+		input:  "projects/12345/secrets/foobar/versions/2",
+		output: "2",
+	},
+	{
+		input: "projects/12345/secrets",
+		output: "1",
+	},
+}
+
+func TestParseSecretVersion(t *testing.T) {
+	for _, test := range secretVersionTests {
+		output := google.ParseSecretVersion(test.input)
+		assert.Equal(t, test.output, output)
 	}
 }
